cdd9ca76811ae9d6eb911b712b03f21aa387d708
unknown <59337871@qq.com>
2019-09-20 20:50:07 +0800

    修改页面

--- js/common.js
+++ js/common.js
@@ -5,3 +5,91 @@ function getUrlParam(name) {
     if (r != null) return unescape(r[2]);
     return null; //返回参数值
 }
+
+function getCode() {
+    var codeNum= false;
+    var time= 60;
+    var clearId; 
+    var mobile= $('.formItem .mobile').val();
+    if(mobile == ''){ //验证手机号码
+        layer.msg('请输入手机号码');
+        return;
+    }else if(!(/^(13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9])\d{8}$/.test(mobile))) {
+        layer.msg('请填写正确的手机号码');
+        return;
+    }else {
+        $('.formItem .getCodeBtn').attr('disabled',"true");
+        clearId= setInterval(timeDown,1000);
+        $.ajax({
+            type: 'post',
+            url: baseUrl+'/api/sms/getCode',
+            data: {"phone":mobile},
+            success: function(res) {
+                if(res.code == '500') {
+                    layer.msg(res.msg);
+                    codeNum= true;
+                }else if(res.code == '200') {
+                    $('.formItem .getCodeBtn').text(time+'s再获取');
+                }
+            }
+        })
+        // 倒计时函数
+        function timeDown() {
+            if(codeNum == false) {
+                if(time == 0) {
+                    $('.formItem>.getCodeBtn').removeAttr('disabled');
+                    $('.formItem>.getCodeBtn').text('获取验证码');
+                    clearInterval(clearId);
+                    time= 60
+               }else {
+                    time--
+                    $('.formItem>.getCodeBtn').text(time+'s再获取');
+               }
+            }else {
+                $('.formItem>.getCodeBtn').removeAttr('disabled');
+                $('.formItem>.getCodeBtn').text('获取验证码');
+                clearInterval(clearId);
+            }
+        }
+    }
+    
+}
+
+
+/* --------------文件上传----------------- */
+function uploadFile(inputEle,containerEle) {
+    fs= inputEle[0].files[0];
+    var reads= new FileReader();
+    reads.readAsDataURL(fs);
+    reads.onload=function (e) {
+        // var targetSrc = this.result;
+        // containerEle.find('.showImg').attr('src',targetSrc);  
+        // containerEle.removeClass('uploadIcon');
+        layer.load(2)      
+    };
+    var formData= new FormData();
+    formData.append('file',fs);
+    $.ajax({
+        url: baseUrl+'/api/upload/upload',
+        // url: test1+'/api/upload/upload',
+        type: 'POST',
+        cache: false, //上传文件不需要缓存
+        data: formData,
+        processData: false, // 告诉jQuery不要去处理发送的数据
+        contentType: false, // 告诉jQuery不要去设置Content-Type请求头
+        success: function (res) {
+            if(res.code == 0) {
+                layer.closeAll('loading')
+                containerEle.find('.showImg').attr('src',res.data);
+            }else {
+                layer.msg(res.msg);
+            }
+        }
+    })
+}
+
+
+/* --------------办理区域----------------- */
+$('.zoomList').on('click','li',function(e){
+    $(this).addClass('checkedItem').siblings('li').removeClass('checkedItem');
+})
